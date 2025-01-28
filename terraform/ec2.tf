64125226ac53bab74df17d699c736a5f94ca899b
hsuliz <hlib.suliz@gmail.com>
2025-01-28 19:44:26 +0100

    Refactoring

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,13 +1,13 @@
-resource "tls_private_key" "oei-key" {
+resource "tls_private_key" "server" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-resource "aws_key_pair" "oei-key-pair" {
-  key_name   = "oei-key-pair"
-  public_key = tls_private_key.oei-key.public_key_openssh
+resource "aws_key_pair" "server" {
+  key_name   = var.key_pair_name
+  public_key = tls_private_key.server.public_key_openssh
   provisioner "local-exec" {
-    command = "echo '${ tls_private_key.oei-key.private_key_pem }' > oei-key-pair.pem"
+    command = "echo '${ tls_private_key.server.private_key_pem }' > '${ var.private_key_name }'"
   }
 }
 
@@ -29,34 +29,31 @@ resource "aws_vpc_security_group_egress_rule" "server" {
 
 resource "aws_vpc_security_group_ingress_rule" "server_allow_ssh" {
   security_group_id = aws_security_group.server.id
-
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 22
-  ip_protocol = "tcp"
-  to_port     = 22
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
 }
 
 resource "aws_vpc_security_group_ingress_rule" "server_allow_http" {
   security_group_id = aws_security_group.server.id
-
-  cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 80
-  ip_protocol = "tcp"
-  to_port     = 80
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
 }
 
 resource "aws_instance" "server" {
   ami = "ami-0e54671bdf3c8ed8d" // Amazon Linux 2023
   instance_type = "t2.micro"
-  key_name      = "oei-key-pair"
-
+  key_name      = aws_key_pair.server.key_name
   vpc_security_group_ids = [aws_security_group.server.id]
 }
 
 resource "local_file" "ansible_inventory" {
   content = templatefile("${ path.module }/templates/inventory.tftpl", {
     ec2_public_ip                = aws_instance.server.public_ip
-    ansible_ssh_private_key_file = "oei-key-pair.pem"
+    ansible_ssh_private_key_file = var.private_key_name
   })
   filename = "inventory"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,15 @@
 variable "aws_vpc_id" {
   type = string
 }
+
+variable "key_pair_name" {
+  type = string
+  description = "Key pair name"
+  default = "taws-key-pair"
+}
+
+variable "private_key_name" {
+  type = string
+  description = "Private key name"
+  default = "private_key.pem"
+}
